f045489937c7b7e426b9f47a088befa7646818e9
Iury Nogueira <ikdn@ic.ufal.br>
2025-01-24 22:33:25 -0300

    Revertendo para o estado do commit anterior

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/global.css'; // Importando os estilos globais
-import '../styles/Home.module.css'; // Importando os estilos específicos do componente, se necessário
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,5 @@
-import dynamic from "next/dynamic";
-import styles from "../styles/Home.module.css";
-
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
-
 function Home() {
-  return (
-    <div className={styles.container}>
-      {/* Título */}
-      <h1 className={styles.title}>😎🤞 Fala fih</h1>
-
-      {/* Frase da música */}
-      <p className={styles.description}>
-        "Oooo pai, só gratidão. Quando te falarem que você não vai vencer..." 🎵
-      </p>
-
-      {/* Player do YouTube */}
-      <div className={styles.playerContainer}>
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=JZo7awP8hc4"
-          playing={false}
-          controls={true}
-          loop={true}
-          width="100%"
-          height="100%"
-          config={{
-            youtube: {
-              playerVars: {
-                start: 20, // Começa aos 20 segundos
-              },
-            },
-          }}
-        />
-      </div>
-
-      {/* Rodapé */}
-      <footer className={styles.footer}>
-        <p>Feito com 😡 pra você!</p>
-      </footer>
-    </div>
-  );
+    return <h1>"😎🤞 fala fih"</h1>
 }
 
 export default Home;
